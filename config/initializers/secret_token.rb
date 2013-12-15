abfc54161a69170ffd79a88c2192354d83e461d1
Bunlong <bunlong.van@gmail.com>
2013-12-15 22:50:24 +0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekhmerFacebookPageTab::Application.config.secret_token = '7d18f489e6d9e72e6f7cea76a54b55358b15ce05ad6d82107d35cd28d7d2679f01a59fcd046d20daad4f0f08a9083dd5aa2a76344cdcaec8a906dc6fff524396'
